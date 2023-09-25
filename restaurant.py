91d363db6d04d063011e4691aeb0287a15f52579
Sherlenbon <dbai22@widc.edu>
2023-09-25 17:54:11 -0500

    Revised by Dan Bai

--- restaurant.py
+++ restaurant.py
@@ -1,6 +1,8 @@
+import random
 restaurants = []
-for i in range(3):
+num = input("How many places are you thinking about for lunch?\n")
+for i in range(int(num)):
     place = input("Enter the name of restuarant " + str(i) + ": ")
     restaurants.append(place)
 
-print(restaurants)
+print(random.choice(restaurants))
