7faa1a9354b6bddc781ae37e764f4d057b0a7d72
Sherlenbon <dbai22@widc.edu>
2023-09-25 17:53:46 -0500

    Revised by Dan Bai

--- rand_num.py
+++ rand_num.py
@@ -1,4 +1,4 @@
 import random
-
-value = random.randint(1, 5)
-print("Choose item #", value)
+num = input("how many things are you trying to choose between? \n")
+value = random.randint(1, int(num))
+print("Choose item #", value)    
