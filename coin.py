6d7996d95b3dae38478bfdf2c10bd7c5b4a3b4c4
Sherlenbon <dbai22@widc.edu>
2023-09-25 17:52:21 -0500

    Revised by Dan Bai

--- coin.py
+++ coin.py
@@ -1,7 +1,12 @@
 import random
+count = 0
+times = input("how many times should the coin be flipped?\n")
+for i in range(int(times)):
+  num = random.randint(1,2)
+  if (num == 1):
+    print("Heads")
+    count = count + 1
+  else:
+    print("Tails")
 
-num = random.randint(1,2)
-if (num == 1):
-  print("Heads")
-else:
-  print("Tails")
+print(count)
