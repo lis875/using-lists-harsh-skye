3c74410f00755611ed0f1495da43b435c0d00584
Sherlenbon <dbai22@widc.edu>
2023-09-25 17:53:25 -0500

    Revised by Dan Bai

--- names.py
+++ names.py
@@ -1,3 +1,5 @@
-names = ['Rick Wash', 'Rushali Bhudeka', 'Mickey Chen', 'Bhavya Shekhawat']
-
-print(names)
+names = ['Rick Wash', 'Rushali Bhudeka', 'Mickey Chen', 'Bhavya Shekhawat','Skye','Harsh Mehta']
+stuff = input("What name do you want to add?\n")
+names.append(stuff)
+names.sort()
+print(names[-1])
